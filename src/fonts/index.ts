801b9568dea782c21e1570983da77ca5969f49e2
Ediongsenyene Joseph <ediongi6@hotmail.com>
2024-01-24 19:57:25 +0100

    setup next/font and chakra-ui

---
+++ src/fonts/index.ts
@@ -0,0 +1,10 @@
+import { Plus_Jakarta_Sans } from 'next/font/google';
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  variable: '--font-pj-sans',
+});
+
+export const fonts = {
+  plusJakartaSans,
+};
---
+++ src/utils/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  fonts: {
+    heading: 'var(--font-pj-sans)',
+    body: 'var(--font-pj-sans)',
+  },
+});
