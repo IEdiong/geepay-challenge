50784b873c9eed076afa7f01bf671ddd5440ebfd
Ediongsenyene Joseph <ediongi6@hotmail.com>
2024-01-25 21:37:31 +0100

    add bar-chart xaxis styles

--- src/components/sales-trends.tsx
+++ src/components/sales-trends.tsx
@@ -1,13 +1,18 @@
 import { Box } from '@chakra-ui/react';
+import SalesChart from './sales-chart';
 
 export default function SalesTrend() {
   return (
     <Box
       w="806px"
       h="374px"
+      paddingBlock="4"
+      paddingInline="5"
       borderRadius="14px"
       bg="gfc.white"
-      border="1px solide #EDF2F7"
-    />
+      border="1px solid #EDF2F7"
+    >
+      <SalesChart />
+    </Box>
   );
 }
