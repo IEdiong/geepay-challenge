f2a4b5a8d0743898ec3ad16ab03ccdb73f5ff1fb
Ediongsenyene Joseph <ediongi6@hotmail.com>
2024-01-25 23:09:29 +0100

    add chart layout

--- src/components/sales-chart.tsx
+++ src/components/sales-chart.tsx
@@ -1,86 +1,158 @@
 'use client';
 
-import { Bar, BarChart, XAxis, ResponsiveContainer } from 'recharts';
+import { useState } from 'react';
+import {
+  Bar,
+  BarChart,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  Cell,
+  CartesianGrid,
+} from 'recharts';
 
 const data = [
   {
     name: 'Jan',
-    uv: 4000,
+    sales: 10000,
     pv: 2400,
     amt: 2400,
   },
   {
     name: 'Feb',
-    uv: 3000,
+    sales: 20000,
     pv: 1398,
     amt: 2210,
   },
   {
     name: 'Mar',
-    uv: 2000,
+    sales: 8000,
     pv: 9800,
     amt: 2290,
   },
   {
     name: 'Apr',
-    uv: 2780,
+    sales: 30000,
     pv: 3908,
     amt: 2000,
   },
   {
     name: 'May',
-    uv: 1890,
+    sales: 10000,
     pv: 4800,
     amt: 2181,
   },
   {
     name: 'Jun',
-    uv: 2390,
+    sales: 45000,
     pv: 3800,
     amt: 2500,
   },
   {
     name: 'Jul',
-    uv: 3490,
+    sales: 10000,
     pv: 4300,
     amt: 2100,
   },
   {
     name: 'Aug',
-    uv: 3490,
+    sales: 25000,
     pv: 4300,
     amt: 2100,
   },
   {
     name: 'Sep',
-    uv: 3490,
+    sales: 35000,
     pv: 4300,
     amt: 2100,
   },
   {
     name: 'Oct',
-    uv: 3490,
+    sales: 5000,
     pv: 4300,
     amt: 2100,
   },
   {
     name: 'Nov',
-    uv: 3490,
+    sales: 30000,
     pv: 4300,
     amt: 2100,
   },
   {
     name: 'Dec',
-    uv: 3490,
+    sales: 28000,
     pv: 4300,
     amt: 2100,
   },
 ];
 
 export default function SalesChart() {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const gradientId = 'colorGradient';
+
+  const handleBarMouseOver = (_: any, index: any) => {
+    setActiveIndex(index);
+  };
+
+  // const handleBarMouseLeave = () => {
+  //   setActiveIndex(null);
+  // };
+
+  const formatYAxis = (tick: any) => tick.toLocaleString();
+
+  // const renderCustomizedLabel = (props: any) => {
+  //   const { x, y, width, height, value } = props;
+  //   const radius = 10;
+
+  //   return (
+  //     <g>
+  //       <circle cx={x + width / 2} cy={y - radius} r={radius} fill="#8884d8" />
+  //       <text
+  //         x={x + width / 2}
+  //         y={y - radius}
+  //         fill="#fff"
+  //         textAnchor="middle"
+  //         dominantBaseline="middle"
+  //       >
+  //         {value.split(' ')[1]}
+  //       </text>
+  //     </g>
+  //   );
+  // };
+
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <BarChart width={150} height={40} data={data}>
+      <BarChart
+        width={150}
+        height={40}
+        data={data}
+        barSize={30}
+        maxBarSize={30}
+      >
+        <defs>
+          <linearGradient id={gradientId} x1="0" y1="0" x2="0" y2="1">
+            <stop offset="15%" stopColor="#34CAA5" stopOpacity={0.8} />
+            <stop
+              offset="85%"
+              stopColor="rgba(52, 202, 165, 0.10)"
+              stopOpacity={0.8}
+            />
+          </linearGradient>
+        </defs>
+
+        <CartesianGrid strokeDasharray="3 3" vertical={false} />
+
+        <YAxis
+          interval="preserveEnd"
+          tickLine={false}
+          fontSize="12px"
+          fontWeight="600"
+          color="#525252"
+          axisLine={false}
+          tickMargin={10}
+          tickFormatter={formatYAxis}
+        />
+
         <XAxis
           dataKey="name"
           tickLine={false}
@@ -90,7 +162,24 @@ export default function SalesChart() {
           axisLine={false}
           tickMargin={12}
         />
-        <Bar dataKey="uv" fill="#8884d8" />
+        <Bar
+          onMouseEnter={handleBarMouseOver}
+          // onMouseLeave={handleBarMouseLeave}
+          dataKey="sales"
+          radius={[20, 20, 0, 0]}
+        >
+          {data.map((entry, index) => (
+            <Cell
+              cursor="pointer"
+              fill={
+                index === activeIndex
+                  ? `url(#${gradientId})`
+                  : 'rgba(52, 202, 165, 0.10)'
+              }
+              key={`cell-${index}`}
+            />
+          ))}
+        </Bar>
       </BarChart>
     </ResponsiveContainer>
   );
--- src/components/sales-trends.tsx
+++ src/components/sales-trends.tsx
@@ -1,9 +1,11 @@
-import { Box } from '@chakra-ui/react';
+'use client';
+import { HStack, Heading, VStack } from '@chakra-ui/react';
 import SalesChart from './sales-chart';
+import { Link } from '@chakra-ui/next-js';
 
 export default function SalesTrend() {
   return (
-    <Box
+    <VStack
       w="806px"
       h="374px"
       paddingBlock="4"
@@ -11,8 +13,24 @@ export default function SalesTrend() {
       borderRadius="14px"
       bg="gfc.white"
       border="1px solid #EDF2F7"
+      rowGap="4"
+      align="stretch"
     >
+      <HStack as="header" justify="space-between">
+        <Heading>Last Orders</Heading>
+        <Link
+          href="/"
+          color="gfc.primary.400"
+          fontWeight="medium"
+          fontSize="lg"
+          lineHeight="26px"
+          transition="all 0.4s ease-in-out"
+          _hover={{ color: 'gfc.primary.500' }}
+        >
+          See All
+        </Link>
+      </HStack>
       <SalesChart />
-    </Box>
+    </VStack>
   );
 }
